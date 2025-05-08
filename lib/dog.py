bdb21d0fdb9d5b0795353324d1a9bef7e3d6a5c3
deenoloo <doreenoloo2@gmail.com>
2025-05-08 07:28:27 -0400

    instance method

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
     # Class body goes here
+    #fido = Dog()
+    #fido.bark()
+
+    #snoopy = Dog()
+    #snoopy.bark()
 
     #Instance method definition
     pass
